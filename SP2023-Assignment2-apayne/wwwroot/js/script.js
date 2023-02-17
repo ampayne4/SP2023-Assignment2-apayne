3f6638fe51f456383897cc5032dd250d38003f99
Alex Payne <ampayne4@crimson.ua.edu>
2023-02-16 20:29:30 -0600

    added I'm feeling lucky button

--- SP2023-Assignment2-apayne/wwwroot/js/script.js
+++ SP2023-Assignment2-apayne/wwwroot/js/script.js
@@ -19,7 +19,7 @@ function apiSearch() {
     .done(function (data) {
       len = data.webPages.value.length;
       for (i = 0; i < len; i++) {
-        results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+        results += "<p><a id = i href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
       }
 
       $('#searchResults').html(results);
@@ -37,4 +37,32 @@ function changeColorJS() {
 function searchJS() {
     apiSearch();
     $('#searchResults').style.visibility = "visible";
+}
+
+function feelingLuckyJS() {
+    var params = {
+        "q": $("#query").val(),
+        "count": "50",
+        "offset": "0",
+        "mkt": "en-us"
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "50d18e11440c477db793d81abe5f61f1");
+        },
+        type: "GET",
+    })
+        .done(function (data) {
+            len = data.webPages.value.length;
+            for (i = 0; i < len; i++) {
+                results += "<p><a id = i href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+            }
+
+        window.location = data.webPages.value[0].url
+        })
+        .fail(function () {
+            alert("error");
+        });
 }
